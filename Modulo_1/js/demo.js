36379a2bab8d4940f9332c492a2edf550eff4688
Mario Cuenca <77672510+MarioCuenca@users.noreply.github.com>
2023-02-02 21:02:46 +0100

    Practica modulo 1

---
+++ Modulo_1/js/demo.js
@@ -0,0 +1,3 @@
+document.getElementById("name").value = "Mi nombre";
+// document.getElementById("lastName").value = "Mi apellido";
+document.getElementById("avatar").src = "img/undraw_male_avatar_g98d.svg"
